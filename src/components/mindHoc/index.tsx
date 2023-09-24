79d105788b35589c09faa3fca6f6338e959e19f8
yijuyang <yijuyang@cvte.com>
2023-09-24 11:55:54 +0800

    feat: 添加mindContext

--- src/components/mindHoc/index.tsx
+++ src/components/mindHoc/index.tsx
@@ -1,8 +1,24 @@
-import { PropsWithChildren, useEffect, useRef, useState } from 'react'
+import { PropsWithChildren, useEffect, useRef, useState, createContext, useContext } from 'react'
+
+type MindContextType = {
+    handleMouseDown: React.MouseEventHandler<HTMLDivElement>;
+    handleMouseMove: (e: MouseEvent) => void;
+    handleMouseUp: React.MouseEventHandler<HTMLDivElement>;
+    currentPosition: { top: number, left: number };
+}
+
+const MindContext = createContext<MindContextType>({
+    handleMouseDown: () => { },
+    handleMouseMove: () => { },
+    handleMouseUp: () => { },
+    currentPosition: { top: 0, left: 0 }
+})
+
 const MindHoc = (props: PropsWithChildren<{}>) => {
     const isMouseDown = useRef(false)
     const [currentPosition, setCurrentPosition] = useState({ top: 0, left: 0 })
     const downPosition = useRef({ top: 0, left: 0 })
+
     const handleMouseMove = (e: MouseEvent) => {
         if (isMouseDown.current) {
             const newTop = e.clientY - downPosition.current.top
@@ -15,6 +31,7 @@ const MindHoc = (props: PropsWithChildren<{}>) => {
             }
         }
     }
+
     useEffect(() => {
         document.addEventListener('mousemove', handleMouseMove)
         return () => {
@@ -36,16 +53,20 @@ const MindHoc = (props: PropsWithChildren<{}>) => {
         isMouseDown.current = false
     }
 
-
     return (
-        <div
-            style={{ position: 'absolute', ...currentPosition }}
-            onMouseDown={handleMouseDown}
-            onMouseUp={handleMouseUp}
-        >
-            {props.children}
-        </div>
+        <MindContext.Provider value={{ handleMouseDown, handleMouseMove, handleMouseUp, currentPosition }}>
+            <div
+                style={{ position: 'absolute', ...currentPosition }}
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+            >
+                {props.children}
+            </div>
+        </MindContext.Provider>
     )
 }
 
-export { MindHoc }
+const useMindContext = () => useContext(MindContext)
+
+export { MindHoc, useMindContext }
+
